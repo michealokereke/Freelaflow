a28cdebb53cdfe46f1ccca5df757d14b3a658c13
micheal okereke <okerekemicheal1219@gmail.com>
2025-10-20 19:24:28 +0100

    init

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `${process.env.BACKEND_URL}/api/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
